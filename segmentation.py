2b7340180e6988946c470246eb592e60b42e316d
Himanshu0ar <himanshu.gla1_cs.aiml24@gla.ac.in>
2026-09-20 19:09:29 +0530

    Phase 2: Person segmentation using rembg (U2Net-based model)

---
+++ segmentation.py
@@ -0,0 +1,21 @@
+﻿from rembg import remove
+from PIL import Image
+import numpy as np
+
+input_path = "input_images/test1.jpeg"
+output_path = "output_images/test1_nobg.png"
+mask_path = "output_images/test1_mask.png"
+
+input_image = Image.open(input_path)
+output_image = remove(input_image)
+output_image.save(output_path)
+
+print("Background removed! Saved to:", output_path)
+
+output_array = np.array(output_image)
+alpha_channel = output_array[:, :, 3]
+
+mask_image = Image.fromarray(alpha_channel)
+mask_image.save(mask_path)
+
+print("Mask save ho gaya:", mask_path)
